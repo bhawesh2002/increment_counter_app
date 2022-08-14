350c690df20d77d0cdb9c9acf8194208122dd925
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-14 18:06:57 +0530

    somechanges to the ui

--- flutter app 1/app_1/lib/main.dart
+++ flutter app 1/app_1/lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'HOT RELOAD'),
     );
   }
 }
@@ -97,6 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
+              style: TextStyle(fontSize: 16),
             ),
             Text(
               '$_counter',
